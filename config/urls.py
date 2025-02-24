c2fb072e0c549328471d587a9b3c0e7682858833
begench2005 <begenchallakov070@gmail.com>
2025-02-24 11:53:08 +0500

    fixed static

--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,7 @@
+import os
+
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
@@ -9,3 +13,7 @@ urlpatterns = [
     path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/v1/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
 ]
+
+if os.getenv("ENVIRONMENT", "local") == "local":
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
